0d5677e91757389c82048415eb04a20ba980351d
Nebojša Jakovljević <nebjak@gmail.com>
2011-10-13 09:26:06 +0200

    izbacivanje nepotrebnih linija u kodu

--- application/views/srb/kontakt/index_page.php
+++ application/views/srb/kontakt/index_page.php
@@ -42,8 +42,7 @@
                             $("#sumbit_button").removeAttr('disabled');
                         }
                     );                    
-                });
-                
+                });                
             });
         </script>
     </head>
