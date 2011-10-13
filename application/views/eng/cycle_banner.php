efebedb25f155b16d804ee0c928674f55beaf78f
Nebojša Jakovljević <nebjak@gmail.com>
2011-10-13 14:31:07 +0200

    ispravljen baner, sada su i slike na 'engleskom'

--- application/views/eng/cycle_banner.php
+++ application/views/eng/cycle_banner.php
@@ -1,9 +1,9 @@
 <a href="<?= base_url() ?>eng/products">
-    <img src="<?= base_url() ?>images/prodaja.jpg" alt="Sale"/>
+    <img src="<?= base_url() ?>images/sale.jpg" alt="Sale" title="Sale"/>
 </a>
 <a href="<?= base_url() ?>eng/service">
-    <img src="<?= base_url() ?>images/servis.jpg" alt="Serice"/>
+    <img src="<?= base_url() ?>images/service.jpg" alt="Serice" title="Service"/>
 </a>
 <a href="<?= base_url() ?>eng/rent">
-    <img src="<?= base_url() ?>images/iznajmljivanje.jpg" alt="Rent" />
+    <img src="<?= base_url() ?>images/rent.jpg" alt="Rent" title="Rent"/>
 </a>
